66b261d9340a1188fbc5697ab7cd433bc56e1e37
gaji12345 <gajimax12345@gmail.com>
2024-01-26 14:06:27 +0530

    create customer table

--- src/main/java/Frist.java
+++ src/main/java/Frist.java
@@ -1,5 +1,20 @@
+import entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import util.FactoryConfiguration;
+
 public class Frist {
     public static void main(String[] args) {
-        System.out.println("gaaji");
+        Customer c1=new Customer();
+        c1.setId("C011");
+        c1.setName("gaaji");
+        c1.setAddress("gaale");
+        c1.setTel(075321456);
+
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(c1);
+        transaction.commit();
+        session.close();
     }
 }
