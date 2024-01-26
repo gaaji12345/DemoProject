9c8768905e3c674777dd6d5799de40110ce82cd0
gaji12345 <gajimax12345@gmail.com>
2024-01-26 14:02:19 +0530

    Customer entity

--- src/main/java/entity/Customer.java
+++ src/main/java/entity/Customer.java
@@ -1,4 +1,23 @@
 package entity;
 
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@Entity
 public class Customer {
+    @Id
+    private String id;
+
+    private  String name;
+    private   String address;
+
+    private int tel;
 }
