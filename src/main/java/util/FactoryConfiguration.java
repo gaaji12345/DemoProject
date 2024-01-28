1871c3897f6797326522203b32cb05c50f104093
gaji12345 <gajimax12345@gmail.com>
2024-01-28 08:26:12 +0530

    add Two Anotation Class

--- src/main/java/util/FactoryConfiguration.java
+++ src/main/java/util/FactoryConfiguration.java
@@ -1,6 +1,8 @@
 package util;
 
 import entity.Customer;
+import entity.Laptop;
+import entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -10,7 +12,7 @@ public class FactoryConfiguration {
     private SessionFactory sessionFactory;
 
     private FactoryConfiguration(){
-        Configuration configuration = new Configuration().configure().addAnnotatedClass(Customer.class);
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(Student.class).addAnnotatedClass(Laptop.class);
         sessionFactory=configuration.buildSessionFactory();
 
     }
